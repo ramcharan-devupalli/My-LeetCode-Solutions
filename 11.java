30cd48ac1d94fe4e65de28618aa794d8d2451a10
ramcharan-devupalli <ramcharan.devupalli9@gmail.com>
2026-07-07 22:54:50 +0530

    07-07-26

---
+++ 11.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1
+        int max_area = 0;
+        while(left < right)
+        {
+            int height = Math.min(height[left], height[right]);
+            int width = right - left;
+            int area = height * width;
+            max_area = Math.max(max_area, area);
+
+            if(height[left] < height[right])
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        return max_area;
+    }
+}
